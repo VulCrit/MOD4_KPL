85f06ab9bdec3cf0a0dbf1c9f4a411aa8bc38622
Zikry <ilovegreen875@gmail.com>
2026-03-28 14:27:15 +0700

    second commit

---
+++ Modul4_103022400080/KodePaket.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class KodePaket
+{
+	private Dictionary<string, string> tabelPaket = new Dictionary<string, string>()
+	{
+		{"Basic", "P201"},
+		{"Standard", "P202"},
+		{"Premium", "P203"},
+		{"Unlimited", "P204"},
+		{"Gaming", "P205"},
+		{"Streaming", "P206"},
+		{"Family", "P207"},
+		{"Business", "P208"},
+		{"Student", "P209"},
+		{"Traveler", "P210"}
+	};
+
+	public string getKodePaket(string namaPaket)
+	{
+		if (tabelPaket.ContainsKey(namaPaket))
+		{
+			return tabelPaket[namaPaket];
+		}
+		else
+		{
+			return "Kode tidak valid";
+		}
+	}
+}
